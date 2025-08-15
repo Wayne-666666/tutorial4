54111d8dca76aa1fd633f4e755502f6eb41f6ddd
Minwei <3183573489@qq.com>
2025-08-16 01:58:09 +1200

    Implemented subtract method and passing tests

--- src/test/java/org/example/CalcTest.java
+++ src/test/java/org/example/CalcTest.java
@@ -4,11 +4,17 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class CalcTest {
-  Calc c = new Calc();
+
+    private final Calc c = new Calc();
 
     @Test
     void testAddition() {
-        assertEquals(2, c.subtract(4,2));
+        assertEquals(4, c.add(2, 2));
     }
 
+    @Test
+    void testSubtraction() {
+        assertEquals(2, c.subtract(4, 2));
+    }
 }
+
